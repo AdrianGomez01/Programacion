12fa99106e749334b052239be7bcdf4b0699a574
Adrian Gomez <adrian.gomez.garlito@gmail.com>
2023-04-17 10:22:27 +0200

    anhadido ejercicio extra

--- Ejemplos/src/Tema7/src/tema7/EjemploXML3.java
+++ Ejemplos/src/Tema7/src/tema7/EjemploXML3.java
@@ -6,7 +6,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
